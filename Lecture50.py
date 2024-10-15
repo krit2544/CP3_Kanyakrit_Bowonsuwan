b66b2dfaf21633df9591cf7e53cb8448ea38b712
Kanyakrit Bowonsuwan <156903315+krit2544@users.noreply.github.com>
2024-10-15 13:57:40 +0700

    Add files via upload

---
+++ Lecture50.py
@@ -0,0 +1,12 @@
+def addNumber(x,y):
+    print(x+y)
+def subNumber(x,y):
+    print(x-y)
+def multiNumber(x,y):
+    print(x*y)
+def divNumber(x,y):
+    print(x/y)
+addNumber(10,5)
+subNumber(10,5)
+multiNumber(10,5)
+divNumber(10,5)
