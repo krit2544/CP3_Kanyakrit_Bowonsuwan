8880e92bbbf088f797d492a5672f8b45a58ab351
Kanyakrit Bowonsuwan <156903315+krit2544@users.noreply.github.com>
2024-10-20 19:11:30 +0700

    Add files via upload

---
+++ Lecture54_Kanyakrit.py
@@ -0,0 +1,38 @@
+
+def login():
+    usernameInput = input("Username : ")
+    passwordInput = input("Password : ")
+    if usernameInput == "admin" and passwordInput == "1234":
+        return True
+    else:
+        return False
+
+def showMenu():
+    print("----- iShop -----")
+    print("1. Vat Calculator")
+    print("2. Price Calculator")
+
+def menuSelect():
+    userSelected = int(input(">>"))
+    return userSelected
+
+def vatCalculator():
+    totalPrice = int(input("Enter You price: "))
+    vat = 7
+    result = totalPrice + (totalPrice * vat / 100)
+    return result
+
+def priceCalculator():
+    price1 = int(input("First Product Price : "))
+    price2 = int(input("Second Product Price : "))
+    return vatCalculator(price1 + price2)
+
+if login() == True :
+    showMenu()
+    x = menuSelect()
+    if x == 1:
+        print(vatCalculator())
+    elif x == 2:
+        print(priceCalculator())
+else:
+    print("Wrong Username or Password, Try again.")
